ba55b2890b33a3c16919c603418fb6a1045d4b7b
Novandi Ramadhan <novandiramadhan80@gmail.com>
2025-04-19 15:02:46 +0700

    feat: Update database constants
    This commit updates the database constants by removing the `TABLE_NAME` constant.

--- app/src/main/java/com/novandiramadhan/petster/common/RoomConstants.kt
+++ app/src/main/java/com/novandiramadhan/petster/common/RoomConstants.kt
@@ -3,6 +3,5 @@ package com.novandiramadhan.petster.common
 object RoomConstants {
     const val DATABASE_NAME = "petster_database"
     const val DATABASE_VERSION = 1
-    const val TABLE_NAME = "favorite_pets"
     const val CHAT_TABLE_NAME = "chat_history"
 }
