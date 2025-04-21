126d4e0558804a9d95b88314a53c88e1dece29b1
Novandi Ramadhan <novandiramadhan80@gmail.com>
2025-04-21 21:24:34 +0700

    feat: Add VolunteerLocation and serialize Volunteer
    This commit adds a `VolunteerLocation` data class with `latitude` and `longitude` properties to represent the location of a volunteer. It also adds `@Serializable` annotation to both `Volunteer` and `VolunteerLocation` classes to enable serialization using `kotlinx.serialization`.

--- app/src/main/java/com/novandiramadhan/petster/domain/model/Volunteer.kt
+++ app/src/main/java/com/novandiramadhan/petster/domain/model/Volunteer.kt
@@ -1,9 +1,19 @@
 package com.novandiramadhan.petster.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Volunteer(
     val uuid: String? = null,
     val name: String? = null,
     val email: String? = null,
     val phoneNumber: String? = null,
     val address: String? = null,
+    val location: VolunteerLocation? = null,
+)
+
+@Serializable
+data class VolunteerLocation(
+    val latitude: Double? = null,
+    val longitude: Double? = null,
 )
