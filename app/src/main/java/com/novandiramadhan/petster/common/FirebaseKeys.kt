dc882fc6b9f5be473e693f16023cbd6d42646ddf
Novandi Ramadhan <novandiramadhan80@gmail.com>
2025-04-23 14:04:33 +0700

    feat: Add POSTS_COLLECTION constant to FirebaseKeys for post management

--- app/src/main/java/com/novandiramadhan/petster/common/FirebaseKeys.kt
+++ app/src/main/java/com/novandiramadhan/petster/common/FirebaseKeys.kt
@@ -6,4 +6,5 @@ object FirebaseKeys {
     const val PET_COLLECTION = "pets"
     const val FAVORITED_PET_COLLECTION = "favorited_pets"
     const val PET_VIEWS_COLLECTION = "pet_views"
+    const val POSTS_COLLECTION = "posts"
 }
