761cf4e3647dbb1746fec4b580663428da167a85
Novandi Ramadhan <novandiramadhan80@gmail.com>
2025-04-24 22:50:29 +0700

    feat: Change post result properties to mutable

--- app/src/main/java/com/novandiramadhan/petster/domain/model/PostResult.kt
+++ app/src/main/java/com/novandiramadhan/petster/domain/model/PostResult.kt
@@ -2,7 +2,7 @@ package com.novandiramadhan.petster.domain.model
 
 data class PostResult(
     val post: Post? = null,
-    val likeCount: Int = 0,
-    val commentCount: Int = 0,
-    val isLiked: Boolean = false
+    var likeCount: Int = 0,
+    var commentCount: Int = 0,
+    var isLiked: Boolean = false
 )
