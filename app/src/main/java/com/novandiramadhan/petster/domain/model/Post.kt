a04d40202ae26bbc9c70d9ceab968455ffc00a8a
Novandi Ramadhan <novandiramadhan80@gmail.com>
2025-04-25 15:47:18 +0700

    feat: Change comments property in Post to be mutable

--- app/src/main/java/com/novandiramadhan/petster/domain/model/Post.kt
+++ app/src/main/java/com/novandiramadhan/petster/domain/model/Post.kt
@@ -8,7 +8,7 @@ data class Post(
     val authorId: String? = null,
     val authorType: String? = null,
     val content: String? = null,
-    val comments: List<PostComment>? = null,
+    var comments: List<PostComment>? = null,
     val likes: List<PostLike>? = null,
     val author: UserResult? = null,
     @ServerTimestamp val createdAt: Date? = null
